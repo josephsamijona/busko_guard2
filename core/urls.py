dcbb192d5917951559b8accc40d7b00a26e849f4
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2024-12-06 09:35:35 -0500

    yo

--- core/urls.py
+++ core/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
    path('admin/', admin.site.urls),
    path('api/', include('accounts.urls')),
    path('api/', include('attendance.urls')),
-   path('api/', include('leaves.urls')),
+   path('api/', include('leave.urls')),
 ]
